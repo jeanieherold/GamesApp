e3679b1e734a47783dcd515abc556fac8d7f3652
Jeanie Herold <jeaniemherold68@gmail.com>
2021-03-08 17:45:57 -0500

    cards displaying with c forloop

--- GamesApp/GamesApp/Models/Card.cs
+++ GamesApp/GamesApp/Models/Card.cs
@@ -8,8 +8,9 @@ namespace GamesApp.Models
     {
         public string Value { get; set; }
         public string Code { get; set; }
-        [JsonPropertyName("img")]
-        public string Image { get; set; }
+
+        [JsonPropertyName("BackImage")]
+        public string BackImage { get; set; }
 
         //overriding and making our own toString method - do this for all is good practice
         public override string ToString() => JsonSerializer.Serialize<Card>(this);
